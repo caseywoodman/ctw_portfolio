fca4008d4ebb218fed5d0e85da4b3e06429dea69
Casey Woodman <caseytwoodman@gmail.com>
2023-03-20 18:17:22 -0500

    nav fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import Header from "./components/Header";
-import Home from "./components/Home";
+
 import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 import "./index.css";
+import Projects from "./components/pages/Projects";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import { useState } from "react";
 
 // React apps typically have a single App component at the very top that can reference other React components.
 // This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
 function App() {
+  const [currentPage, setCurrentPage] = useState("Projects");
+
+  const renderPage = () => {
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div class="bg-[#1C2541] h-screen flex flex-col">
-      <Header />
-      <Home />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      <div className="w-full text-[#6FFFE9] flex-1 flex flex-col">{renderPage()}</div>
+
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Nav from "./Nav";
 
-function Header() {
+function Header({ currentPage, handlePageChange }) {
   return (
     <div className="bg-[#5BC0BE] p-4">
       <h2 className="text-center text-3xl font-bold">CASEY WOODMAN</h2>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
     </div>
   );
 }
