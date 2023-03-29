9be4cebca881fc66d58087aa689b8c9391a22149
Casey Woodman <caseytwoodman@gmail.com>
2023-03-28 21:34:28 -0500

    finished

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Headshot from "./assets/Woodman Headshot.jpg";
 
 function About() {
   return (
@@ -7,7 +6,7 @@ function About() {
       <h1 className="text-center text-3xl font-bold">ABOUT ME</h1>
       <div className="flex justify-center">
         <div className="p-4 w-3/12">
-          <img src={Headshot} alt="headshot" className="border-8 border-[#5BC0BE]" />
+          <img src="./assets/Woodman Headshot.jpg" alt="headshot" className="border-8 border-[#5BC0BE]" />
         </div>
         <div className="p-4 w-7/12 flex flex-wrap flex-1">
           <div className="flex flex-col m-2 bg-[#5BC0BE] text-black p-4 min-w-[40%] flex-1">
